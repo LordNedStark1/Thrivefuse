b82896a7b67da6ce0cb07795779d32f7f047978a
ned <osisiogubenjamin1@gmail.com>
2024-02-21 13:57:40 +0100

    first push

---
+++ src/pages/StudentDashboard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import SideBar from "../components/SideBar";
+import Profile from "./Profile";
+import "../css/StudentDashboard.css";
+import DateRangeCalendarCalendarsProp from "../components/Calendar";
+import Courses from "./Courses";
+
+function StudentDashboard() {
+  return (
+    <div>
+      <div className="student-dashboard-main-container flex">
+        <div className="ml-7 mt-8">
+          <SideBar/>
+        </div>
+        <div className="dashboard-display-section">
+        <div className="ml-7 mt-5">
+            <h2 className=" text-xl font-bold">Dashboard</h2>
+          </div>
+          {/* <Profile /> */}
+          <Courses/>
+        </div>
+        <DateRangeCalendarCalendarsProp/>
+      </div>
+    </div>
+  );
+}
+
+export default StudentDashboard;
